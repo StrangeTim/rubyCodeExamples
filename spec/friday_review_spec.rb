771798d34a9f863bd50b15e66318fe885e05aafd
Steven Bussert <stevenbussert@gmail.com>
2015-04-24 10:32:22 -0700

    Add a new spec for more complex testing.

--- spec/friday_review_spec.rb
+++ spec/friday_review_spec.rb
@@ -5,4 +5,7 @@ describe('String#friday_review') do
   it("Returns a count of 1 or zero for the presence of a sample string within a test string.") do
     expect("happy".friday_review("happy")).to(eq(1))
   end
+  it("Returns the correct count for the number of times a sample string appears within a test string.") do
+    expect("happy".friday_review("I am happy, happy")).to(eq(2))
+  end
 end
