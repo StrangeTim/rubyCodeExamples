11293f98815c6b7fa5e60e4568285f99de5e7d32
Brent Bailey & Steve Bussert <student@epicodus.com>
2015-04-27 11:02:01 -0700

    Pass deletion spec, update grab method

--- lib/my_hash.rb
+++ lib/my_hash.rb
@@ -1,41 +1,48 @@
 require ('pry')
 
 class My_Hash
-  stored_keys = []
-  stored_values = []
+
 
   define_method(:initialize) do |key, value|
-    @key = key
-    @value = value
-    store(@key, @value)
+    @stored_keys = []
+    @stored_values = []
+    store(key, value)
   end
 
   define_method(:store) do |key, value|
-    if stored_keys[0] == nil
-      stored_keys[0] = key
-      stored_values[0] = value
+    if @stored_keys[0] == nil
+      @stored_keys[0] = key
+      @stored_values[0] = value
+    else
+    @stored_keys.push(key)
+    @stored_values.push(value)
     end
-    stored_keys.push(key)
-    stored_keys.push(value)
+    puts @stored_keys
+    puts @stored_values
   end
 
   define_method(:remove) do |key|   #removes based only on key value
-    to_delete = stored_keys.find_index(key)
-    stored_keys.delete_at(to_delete)
-    stored_values.delete_at(to_delete)
+#    to_delete = stored_keys.find_index(key)
+    @stored_values.delete_at(@stored_keys.find_index(key))
+    @stored_keys.delete_at(@stored_keys.find_index(key))
+
   end
 
   define_method(:grab) do |key|
-    return_key = stored_keys.find_index(key)
-    return_value = stored_values[return_key]
-    return_value
+    if @stored_keys.include?(key) == false
+      "That key doesn't exist."
+    else
+      return_key = @stored_keys.find_index(key)
+      return_value = @stored_values[return_key]
+      return_value
+    end
   end
 
-  define_method(:key_exists) do |key|
-    stored_keys.include?(key)
+  define_method(:key_exists?) do |key|
+    @stored_keys.include?(key)
   end
 
-  define_method(:value_exists) do
+  define_method(:value_exists?) do
 
   end
 
--- spec/my_hash_spec.rb
+++ spec/my_hash_spec.rb
@@ -1,12 +1,23 @@
 require ('rspec')
 require ('my_hash')
+require ('pry')
 
 describe(My_Hash) do
   describe("#grab") do
     it("retrieves a stored value by its key") do
       test_hash = My_Hash.new(nil, nil)
-      test_hash.store("kitten", "cute")
+      test_hash.store("kitten", "cute")   # oops, we tested two specs in one!
       expect(test_hash.grab("kitten")).to(eq("cute"))
+    end
+  end
+
+  describe("#remove") do
+    it("Removes key/value pair based on key from current hash") do
+      initial_hash = My_Hash.new("Dog", "Furry")
+      initial_hash.store("Cat", "Furry")
+      initial_hash.remove("Dog")
+      expect(initial_hash.grab("Dog")).to(eq("That key doesn't exist."))
+# binding.pry
     end
   end
 end
