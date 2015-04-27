d23550bb74007d68e2f0bb19991f61342a1dadce
Steve Bussert & Brent Bailey <student@epicodus.com>
2015-04-27 10:17:52 -0700

    pass first spec

--- lib/my_hash.rb
+++ lib/my_hash.rb
@@ -1,21 +1,38 @@
 require ('pry')
 
 class My_Hash
+  stored_keys = []
+  stored_values = []
+
   define_method(:initialize) do |key, value|
     @key = key
     @value = value
+    store(@key, @value)
   end
 
-  define_method(:store) do
-
+  define_method(:store) do |key, value|
+    if stored_keys[0] == nil
+      stored_keys[0] = key
+      stored_values[0] = value
+    end
+    stored_keys.push(key)
+    stored_keys.push(value)
   end
 
-  define_method(:grab) do
-
+  define_method(:remove) do |key|   #removes based only on key value
+    to_delete = stored_keys.find_index(key)
+    stored_keys.delete_at(to_delete)
+    stored_values.delete_at(to_delete)
   end
 
-  define_method(:key_exists) do
+  define_method(:grab) do |key|
+    return_key = stored_keys.find_index(key)
+    return_value = stored_values[return_key]
+    return_value
+  end
 
+  define_method(:key_exists) do |key|
+    stored_keys.include?(key)
   end
 
   define_method(:value_exists) do
@@ -33,5 +50,6 @@ class My_Hash
   define_method(:merge) do
 
   end
+
 
 end
--- spec/my_hash_spec.rb
+++ spec/my_hash_spec.rb
@@ -2,11 +2,11 @@ require ('rspec')
 require ('my_hash')
 
 describe(My_Hash) do
-  describe("#fetch") do
+  describe("#grab") do
     it("retrieves a stored value by its key") do
-      test_hash = My_Hash.new()
+      test_hash = My_Hash.new(nil, nil)
       test_hash.store("kitten", "cute")
-      expect(test_hash.fetch("kitten")).to(eq("cute"))
+      expect(test_hash.grab("kitten")).to(eq("cute"))
     end
   end
 end
