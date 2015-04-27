edb39023043e0202574d78f73cb1f6751c8f2349
Brent Bailey & Steve Bussert <student@epicodus.com>
2015-04-27 11:45:41 -0700

    Add testing functionality for scalene triangles.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -4,8 +4,24 @@ class Triangle
     @side2 = side2
     @side3 = side3
   end
-  define_method(:equilateral) do
+  define_method(:equilateral?) do
     if @side1 == @side2 && @side2 == @side3
+      true
+    else
+      false
+    end
+  end
+  define_method(:isosceles?) do
+    if @side1 == @side2 && @side3 == @side2
+      false
+    elsif @side1 == @side2 || @side2 == @side3 || @side3 == @side1
+      true
+    else
+      false
+    end
+  end
+  define_method(:scalene?) do
+    if @side1 != @side2 && @side1 != @side3 && @side2 != @side3
       true
     else
       false
