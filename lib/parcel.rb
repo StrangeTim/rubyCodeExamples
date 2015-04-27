e7a549d8f21076d166dbdcccea2b08f9bb505013
Steve Bussert & Brent Bailey <student@epicodus.com>
2015-04-27 14:25:15 -0700

    Get first spec working for volume calculation.

---
+++ lib/parcel.rb
@@ -0,0 +1,14 @@
+require 'pry'
+
+class Parcel
+  define_method(:initialize) do |length, width, height, weight|
+    @length = length
+    @width = width
+    @height = height
+    @weight = weight
+  end
+
+  define_method(:volume) do
+    volume = @length * @width * @height
+  end
+end
