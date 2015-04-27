a3bd674e98b3941ee9143d3519a64c7684b93758
Brent Bailey & Steve Bussert <student@epicodus.com>
2015-04-27 11:19:11 -0700

    contain falsity spec for equilateral and true/false specs for isosceles

--- spec/triangle_spec.rb
+++ spec/triangle_spec.rb
@@ -3,15 +3,27 @@ require('triangle')
 
 describe(Triangle) do
   describe('#equilateral') do
-  it('tests to see if each side of the triangle is equal') do
+  it('returns true if each side of the triangle is equal') do
     test_equilateral = Triangle.new(2, 2, 2)
     expect(test_equilateral.equilateral).to(eq(true))
+  end
+  it('returns false if each side of the triangle is not equal') do
+    test_equilateral2 = Triangle.new (1, 1, 2)
+    expect(test_equilateral2.equilateral).to(eq(false))
+  end
+  end
 
-  end
-  end
   describe('#isosceles') do
-
+  it('returns true if the triangle is isosceles') do
+    test_isosceles = Triangle.new(2, 2, 3)
+    expect(test_isosceles.isosceles).to(eq(true))
   end
+  it('returns false if the triangle is not isosceles') do
+    test_isosceles2 = Triangle.new (1, 1, 1) do
+    expect(test_isosceles2.isosceles).to(eq(false))
+  end
+  end
+
   describe('#scalene') do
 
   end
