e1a7d4b7c05a3fb9db7e09bd048e3ea060d4782c
Steve Bussert & Brent Bailey <student@epicodus.com>
2015-04-27 14:53:27 -0700

    add integration spec for cost path

---
+++ spec/parcel_integration_spec.rb
@@ -0,0 +1,17 @@
+require 'capybara/rspec'
+require  './app'
+Caybara.app = Sinatra::Application
+set(:show_exceptions, false)
+
+describe('Package volume and cost calculation spec program paths.', {:type => :feature}) do
+  it ('Inputs package parameters and calculates cost, displayed correctly') do
+    visit('/')
+    fill_in('length', :with => 3)
+    fill_in('width', :with => 5)
+    fill_in('height', :with => 5)
+    fill_in('weight', :with => 10)
+    fill_in('distance', :with => 250)
+    click_button("Submit")
+    expect(page).to have_content("Your cost is: $27.75.")
+  end
+end
