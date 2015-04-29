79c4f730348341394e8b06e6fb9d8bea4618acac
Steve & Jess <student@epicodus.com>
2015-04-29 10:22:57 -0700

    allow user to find car by identification

--- lib/car_desc.rb
+++ lib/car_desc.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Car
   @@cars = []
 
@@ -6,6 +8,32 @@ class Car
     @model = car_model
     @year = year
     @color = color
+    @id = @@cars.length().+(1)
+    # if @@cars.length() > 0
+    #   last_id = @@cars[-1].get_id()
+    #   @id = last_id + 1
+    # else
+    #   @id = 1
+    # end
+  end
+
+  define_singleton_method(:all) do
+    @@cars
+  end
+
+  define_singleton_method(:get_by_id) do |desired_car|
+
+    @@cars.each() do |car|
+      if car.get_id() == desired_car
+        return car
+      else
+        "No match found"
+      end
+    end
+  end
+
+  define_singleton_method(:clear) do
+    @@cars = []
   end
 
   define_method(:add) do
@@ -27,4 +55,9 @@ class Car
   define_method(:get_color) do
     @color
   end
+
+  define_method(:get_id) do
+    @id
+  end
+
 end
--- spec/car_desc_spec.rb
+++ spec/car_desc_spec.rb
@@ -2,10 +2,27 @@ require('rspec')
 require('car_desc')
 
 describe(Car) do
+
+  before() do
+    Car.clear
+  end
+
   describe('#add') do
     it("adds a car to the class array") do
       test_car = Car.new("Toyota", "Corolla", "1995", "neon green")
       expect(test_car.add()).to(eq([test_car]))
     end
   end
+
+  describe('.get_by_id') do
+    it('checks to ensure proper id incrementation') do
+      test_car = Car.new("Toyota", "Corolla", "1995", "neon green")
+      test_car.add()
+      test_car2 = Car.new("Toyota", "Corolla", "1996", "neon green")
+      test_car2.add()
+      expect(Car.get_by_id(1)).to(eq(test_car))
+    end
+  end
+
+  
 end
