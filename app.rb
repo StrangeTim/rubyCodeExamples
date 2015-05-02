7f3f98a709c4ae13228d7e4aed7700ef894ece6f
Steve <strang3tim@gmail.com>
2015-05-01 20:33:55 -0700

    Work In Progress. Break save.

--- app.rb
+++ app.rb
@@ -7,6 +7,7 @@ also_reload("lib/**/*.rb")
 @all_words = {}
 @dictionary = {}
 @word = " "
+@current_word = nil
 
 
 get('/') do
@@ -16,7 +17,8 @@ get('/') do
   erb(:index)
 end
 
-get('/success') do
+post('/') do
+
   @word = params.fetch('word').downcase
   @message = ""
   @choice = params.fetch('drop_choice')
@@ -25,18 +27,35 @@ get('/success') do
     word_to_add = Word.new(@word)
     word_to_add.dictionary_add
     word_to_add.list_add
-    erb(:success)
   elsif @choice == "2"
     @message = "The delete function is still under construction."
-    erb(:success)
   elsif @choice == "3"
-    @all_words = Word.dictionary.to_a
+    @current_word = Word.all_words
     erb(:display)
   end
+  @dictionary = Word.dictionary
+  @all_words = Word.all_words
+  @words_only = @dictionary.keys
+  erb(:index)
 end
 
 get('/display/:id') do
+  @dictionary = Word.dictionary
   @all_words = Word.all_words
-  @display_word = @all_words.fetch(params.fetch('id').to_i)
+  @words_only = @dictionary.keys
+
+  @current_word = @all_words.fetch(params.fetch('id').to_i)
+  erb(:display)
+end
+
+post('/display/:id') do
+  @dictionary = Word.dictionary
+  @all_words = Word.all_words
+  @words_only = @dictionary.keys
+
+  @definition = params.fetch('definition')
+  new_def = Definition.new(@definition)
+  @current_word = @all_words.fetch(params.fetch('id').to_i)
+  @current_word.add_def(new_def)
   erb(:display)
 end
