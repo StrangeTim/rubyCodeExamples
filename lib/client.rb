8b601245ad029b5c0ff6bec5f0453684b3fd2b4c
Steve Bussert <strang3tim@Gmail.com>
2015-05-10 11:41:48 -0700

    Create initial files

---
+++ lib/client.rb
@@ -0,0 +1,36 @@
+require 'pry'
+
+class Stylist
+  attr_reader(:stylist_id, :stylist_name)
+
+  define_method(:initialize) do |variables|
+
+
+  end
+
+
+  define_singleton_method(:all) do
+    #words
+  end
+
+
+  define_singleton_method(:this_stylist) do |stylist_id|
+    #words
+  end
+
+
+  define_method(:save) do
+    #words
+  end
+
+
+  define_method(:update) do |info|
+    #words
+  end
+
+
+  define_method(:delete) do
+    #words
+  end
+
+end
