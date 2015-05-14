63920bc564c8d1e1cf757d2ec1bb056d8d59cdbe
Steve Bussert <strang3tim@Gmail.com>
2015-05-13 22:39:54 -0700

    Switch most processes to ActiveRecord.

--- lib/client.rb
+++ lib/client.rb
@@ -1,36 +1,3 @@
-require 'pry'
-
-class Stylist
-  attr_reader(:stylist_id, :stylist_name)
-
-  define_method(:initialize) do |variables|
-
-
-  end
-
-
-  define_singleton_method(:all) do
-    #words
-  end
-
-
-  define_singleton_method(:this_stylist) do |stylist_id|
-    #words
-  end
-
-
-  define_method(:save) do
-    #words
-  end
-
-
-  define_method(:update) do |info|
-    #words
-  end
-
-
-  define_method(:delete) do
-    #words
-  end
-
+class Client < ActiveRecord::Base
+  belongs_to :stylist
 end
